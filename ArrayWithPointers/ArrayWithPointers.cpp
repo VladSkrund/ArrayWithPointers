95995869c5488c58e9db16acf97a3401d5bcb2dc
VladSkrund <vladskrund20@gmail.com>
2022-10-23 10:40:12 +0300

    Добавьте файлы проекта.

---
+++ ArrayWithPointers/ArrayWithPointers.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+int getIndexMaxItem(int* arr, int size);
+
+int main()
+{
+	int size = 0;
+	cout << "Enter array size:";
+	cin >> size;
+
+	int* array = new int[size];
+	int index = 0;
+
+	for (int* ptr = array; ptr <= &array[size - 1]; ptr++) {
+		cout << "number[" << index << "]:";
+		cin >>* ptr;
+		index++;
+	}
+
+	cout << "Max index = " << getIndexMaxItem(array, size);
+
+	return 0;
+}
+
+int getIndexMaxItem(int* arr, int size) {
+	int indexMaxValue = 0;
+	int tempMaxValue = *arr;
+	int index = 0;
+
+	for (int* ptr = arr; ptr <= &arr[size - 1]; ptr++) {
+		if (*ptr > tempMaxValue) {
+			indexMaxValue = index;
+			tempMaxValue = *ptr;
+		}
+		//cout << "pointer :" << ptr << endl;
+		//cout << "value :" << * ptr << endl;
+
+		index++;
+	}
+
+	return indexMaxValue;
+}
+
